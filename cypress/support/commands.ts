8f7e3f2a348ed133db465e6b49a8e248711a76ad
ducnguyen <ducnn.230288@gmail.com>
2023-05-18 09:44:09 +0700

    add eslint and prettier

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -17,7 +17,7 @@ Cypress.Commands.add(
 );
 Cypress.Commands.add('typeRandom', { prevSubject: 'element' }, (element, text, type): any => {
   const random = '_RANDOM_';
-  if (text.indexOf(random) > -1) {
+  if (text.indexOf(random) > -1 || ['format'].indexOf(type || '') > -1) {
     switch (type) {
       case 'number':
         text = text.replace(random, faker.finance.account(length).toString());
@@ -27,6 +27,9 @@ Cypress.Commands.add('typeRandom', { prevSubject: 'element' }, (element, text, t
         break;
       case 'email':
         text = text.replace(random, faker.internet.email().toLowerCase());
+        break;
+      case 'format':
+        text = faker.helpers.replaceSymbols(text);
         break;
       default:
         text = text.replace(random, faker.random.word());
